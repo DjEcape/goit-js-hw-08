725de47e528eb19edcfebbecdad4bf97ffece7b6
Vitaliy Opanashchuk <booking.opanashchuk@gmail.com>
2022-08-09 23:59:45 +0300

    push

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
+import { galleryItems } from './gallery-items';
+
+
+import SimpleLightbox from 'simplelightbox';
+
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(
+      ({preview, original, description }) =>
+        `<a class="gallery__item" href="${original}"> 
+    <img class="gallery__image"
+    src="${preview}"
+    alt ="${description}"/>
+    </a>`
+    )
+    .join("")
+);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  close: false,
+  showCounter: false,
+  animationSpeed: 200,
+
+});
