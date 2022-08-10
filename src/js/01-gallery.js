f48fc644482d71cd67b804d4e10d75619ca874a6
Vitaliy Opanashchuk <booking.opanashchuk@gmail.com>
2022-08-11 00:29:22 +0300

    )

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,6 @@ import { galleryItems } from './gallery-items';
 
 import SimpleLightbox from 'simplelightbox';
 
-// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
 
