0d1ff439dd3413070417465c01ff46c414ad58ad
Vitaliy Opanashchuk <booking.opanashchuk@gmail.com>
2022-08-14 23:13:01 +0300

    fix 2nd homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
 import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const LOCAL_STORAGE_CURRENT_TIME = 'videoplayer-current-time';
 
 const player = new Vimeo('vimeo-player');
 
 player.on('loaded', () => {
-  let currentTime = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+  let currentTime = localStorage.getItem(LOCAL_STORAGE_CURRENT_TIME) || 0;
   player.setCurrentTime(currentTime);
 });
 
 player.on(
   'timeupdate',
   throttle(({ seconds }) => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, seconds);
+    localStorage.setItem(LOCAL_STORAGE_CURRENT_TIME, seconds);
   }, 1000)
 );
